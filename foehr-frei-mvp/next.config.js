334dc1fa2e3372977325a89290b2f268731bd7c4
Fruchtisafti <glegomoe@googlemail.com>
2025-08-13 08:39:37 +0200

    Update next.config.js

--- foehr-frei-mvp/next.config.js
+++ foehr-frei-mvp/next.config.js
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // appDir ist in Next 14 eh Standard – raus damit:
-  // experimental: { appDir: true },
-  typescript: { ignoreBuildErrors: true }, // TS-Fehler blockieren nicht
-  eslint: { ignoreDuringBuilds: true },    // ESLint blockiert nicht
+  typescript: { ignoreBuildErrors: true },
+  eslint: { ignoreDuringBuilds: true },
 };
 module.exports = nextConfig;
